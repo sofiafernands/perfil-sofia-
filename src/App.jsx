f4fcd5f0ae1929969a973d22d8059d5ffd4a0814
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-11-28 22:38:49 +0000

    estilos unicamente con css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <h1 className="App-title">Sofia Fernandes</h1>
+        <p className="App-subtitle">Desarrolladora Web Full-Stack</p>
       </header>
+
+      <section className="Skills">
+        <h2>Habilidades</h2>
+        <ul>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Tailwind</li>
+          <li>Python</li>
+          <li>Bootstrap</li>
+          <li>Git</li>
+        </ul>
+      </section>
+
+      <section className="Projects">
+     
+
+      <div className="Project">
+          <h3>HOMYO</h3>
+          <p>Descripción del proyecto.</p>
+        </div>
+
+        <div className="Project">
+          <h3>DAVIDRIPHONES</h3>
+          <p>Descripción del proyecto.</p>
+        </div>
+
+        <div className="Project">
+          <h3>MY CHECK LIST</h3>
+          <p>Descripción del proyecto.</p>
+        </div>
+
+        <div className="Project">
+          <h3>API STARWARS</h3>
+          <p>Descripción del proyecto.</p>
+        </div>
+      </section>
     </div>
   );
 }
